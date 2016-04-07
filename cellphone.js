075687b71b42962ddbeeb4b0fadb96a3d563812f
Evans-Munatsa <gagamel.em@gmail.com>
2016-04-07 11:34:35 +0200

    added second functionality

--- cellphone.js
+++ cellphone.js
@@ -1,21 +1,24 @@
 var walter = "It was 67MB. Is that everything? I’m busy, you know.";
-
-var cutting = walter.replace("It was ", " ").replace(". Is that everything? I’m busy, you know.", " ").split(",");
+var cutting = walter.replace("It was ", " ").replace("MB. Is that everything? I’m busy, you know.", " ").split(",");
+var name = 'Walter ';
 
 var jesse = "Damn, it was like 300MB. Dat Snaptalk is a data hog, yo.";
-var cutting2 = jesse.replace("Damn, it was like ", " ").replace(". Dat Snaptalk is a data hog, yo.", " ").split(",");
+var cutting2 = jesse.replace("Damn, it was like ", " ").replace("MB. Dat Snaptalk is a data hog, yo.", " ").split(",");
+var name2 = "Jesse ";
 
 var saul = "I think 283MB. That’s what the guy at the store said. I think it means MegaBites.";
-var cutting3 = saul.replace("I think ", " ").replace(". That’s what the guy at the store said. I think it means MegaBites.", " ").split(",");
+var cutting3 = saul.replace("I think ", " ").replace("MB. That’s what the guy at the store said. I think it means MegaBites.", " ").split(",");
+var name3 = "Saul ";
 
 var Gus = "Around 150MB. I have the receipts if you need more precise figures.";
-var cutting4 = Gus.replace("Around", "").replace(". I have the receipts if you need more precise figures.", "").split(",");
+var cutting4 = Gus.replace("Around", "").replace("MB. I have the receipts if you need more precise figures.", "").split(",");
+var name4 = "Gus ";
 
 var pushed = [];
 pushed.push(cutting, cutting2, cutting3, cutting4);
 
-// console.log(cutting);
-// console.log(cutting2);
-// console.log(cutting3);
-// console.log(cutting4);
+var max = Math.max.apply(null, pushed);
+var min = Math.min.apply(null, pushed);
 console.log(pushed);
+console.log(name2 + max + "MB");
+console.log(name + min + "MB");
